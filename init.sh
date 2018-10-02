c9d431b175ec40f6eaea1160f1990cd76c63d253
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-02 03:32:30 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-/verium/1wayminer/cpuminer -o stratum+tcp://vrm.poolium.win:3332 -O drevilish.odroidSwarm:steve &
-/verium/3wayminer/cpuminer -o stratum+tcp://vrm.poolium.win:3332 -O drevilish.odroidSwarm:steve &
+
+/verium/3wayminer/cpuminer -o stratum+tcp://vrm.poolium.win:3332 -O drevilish.odroidSwarm:steve --cpu-priority 4 -t 3 --cpu-affinity 0x00FF &
+
+/verium/1wayminer/cpuminer -o stratum+tcp://vrm.poolium.win:3332 -O drevilish.odroidSwarm:steve --cpu-priority 1 -t 5 -b 4049 &
